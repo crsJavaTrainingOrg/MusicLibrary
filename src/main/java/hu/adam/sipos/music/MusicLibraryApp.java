ffb9d67bb79d92745cfdaf8a143d53e74552621c
siposadam97 <siposadam.97@gmail.com>
2018-04-13 20:46:53 +0200

    Initial commit

---
+++ src/main/java/hu/adam/sipos/music/MusicLibraryApp.java
@@ -0,0 +1,7 @@
+package hu.adam.sipos.music;
+
+public class MusicLibraryApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/hu/adam/sipos/music/domain/LibraryTest.java
@@ -0,0 +1,10 @@
+package hu.adam.sipos.music.domain;
+
+import org.junit.Test;
+
+public class LibraryTest {
+    @Test
+    public void shouldCreateAnEmptyLibrary() {
+
+    }
+}
